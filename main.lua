a54a52cff229b61a8cd629c575c01754956bfff3
4bx9 <164088226+4bx9@users.noreply.github.com>
2024-04-19 17:53:41 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,34 +2,5 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 -- clipboard abuser 1.0 (fully by 4bx9)
 
 --actual boom boom part
-while wait(0.3) do
 setclipboard("4bx9 on top")
-wait(0.5)
-setclipboard("4bx9 owned you lolz!")
-wait(0.5)
-while wait(0.3) do
-setclipboard("4bx9 on top")
-wait(0.5)
-setclipboard("4bx9 owned you lolz!")
-wait(0.5)
-while wait(0.3) do
-setclipboard("4bx9 on top")
-wait(0.5)
-setclipboard("4bx9 owned you lolz!")
-wait(0.5)
-while wait(0.3) do
-setclipboard("4bx9 on top")
-wait(0.5)
-setclipboard("4bx9 owned you lolz!")
-wait(0.5)
-while wait(0.3) do
-setclipboard("4bx9 on top")
-wait(0.5)
-setclipboard("4bx9 owned you lolz!")
-wait(0.5)
-while wait(0.3) do
-setclipboard("4bx9 on top")
-wait(0.5)
-setclipboard("4bx9 owned you lolz!")
-wait(0.5)
-end
+toclipboard("owned by 4bx9 lolz!!")
