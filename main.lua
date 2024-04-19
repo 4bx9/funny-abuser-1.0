dcc2707d4fac3cce0939d9200f41621dd47d059e
4bx9 <164088226+4bx9@users.noreply.github.com>
2024-04-19 11:59:07 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,32 @@
---holdon mane give me time to finish this
+-- clipboard abuser 1.0 (fully by 4bx9)
+
+local 4bx9 = 4bx9 on top
+local 4bx92 = 4bx9 owned you lolz!!
+
+--actual boom boom part
+while wait(0.3) do
+setclipboard(4bx9)
+wait(0.5)
+setclipboard(4bx92)
+wait(0.5)
+setclipboard(4bx9)
+wait(0.5)
+setclipboard(4bx92)
+wait(0.5)
+setclipboard(4bx9)
+wait(0.5)
+setclipboard(4bx92)
+wait(0.5)
+setclipboard(4bx9)
+wait(0.5)
+setclipboard(4bx92)
+wait(0.5)
+setclipboard(4bx9)
+wait(0.5)
+setclipboard(4bx92)
+wait(0.5)
+setclipboard(4bx9)
+wait(0.5)
+setclipboard(4bx92)
+wait(0.5)
+end
