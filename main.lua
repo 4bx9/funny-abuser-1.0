9388908de8df36d1fcb54e04a18e33fb126c3868
4bx9 <164088226+4bx9@users.noreply.github.com>
2025-02-19 19:59:39 +0100

    die
    die

--- main.lua
+++ main.lua
@@ -1,6 +1,1 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
--- clipboard abuser 1.0 (fully by 4bx9)
-
---actual boom boom part
-setclipboard("4bx9 on top")
-toclipboard("owned by 4bx9 lolz!!")
+-- die
